85454adcb5fed2e118d9a4a49e0e931f78bf4b70
L.S.S <30771484+lss4@users.noreply.github.com>
2019-09-21 16:28:36 +0900

    maple-common: Adjust for building AOSP Extended.

--- maple_common.mk
+++ maple_common.mk
@@ -6,12 +6,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/sony/maple-common/device.mk)
 
 ### BOOTANIMATION
-# vendor/lineage/config/common_full_phone.mk
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-# vendor/lineage/config/common.mk
-TARGET_BOOTANIMATION_HALF_RES := true
+# vendor/aosp/config/bootanimation.mk
+TARGET_BOOT_ANIMATION_RES := 1080
 
-### Carbon
-$(call inherit-product, vendor/carbon/config/gsm.mk)
-$(call inherit-product, vendor/carbon/config/common.mk)
+### AOSP Extended
+$(call inherit-product, vendor/aosp/config/common.mk)
